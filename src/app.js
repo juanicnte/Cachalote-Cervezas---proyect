1616b187b9ef31cfbc644016338a779d0474531c
juanicnte <juanmacen547@gmail.com>
2022-07-25 18:22:05 -0300

    first commit

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const path = require ('path');
 const app = express();
 const publicPath = path.resolve(__dirname, '../public');
 app.use(express.static(publicPath)) 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
     console.log('servidor corriendo...');
 });
 app.get ('/', (req, res) => {
